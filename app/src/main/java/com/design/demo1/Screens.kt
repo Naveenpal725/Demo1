d5dc990908a779a603ae58ffa7da9a59d4be5897
ayush <ayushkumar605575@gmail.com>
2024-02-27 22:41:26 +0530

    Added a Button

--- app/src/main/java/com/design/demo1/Screens.kt
+++ app/src/main/java/com/design/demo1/Screens.kt
@@ -4,15 +4,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
