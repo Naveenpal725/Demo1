a1adff28053b924b4e8c38d1eee22543d3a42cce
ayush <ayushkumar605575@gmail.com>
2024-02-27 23:13:38 +0530

    Demo App

--- app/src/main/java/com/design/demo1/MainActivity.kt
+++ app/src/main/java/com/design/demo1/MainActivity.kt
@@ -3,17 +3,10 @@ package com.design.demo1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.design.demo1.ui.theme.Demo1Theme
 
 class MainActivity : ComponentActivity() {
@@ -26,29 +19,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Ayush kumar CSE AI")
+                    Screen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Screen()
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Demo1Theme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/design/demo1/Screens.kt
+++ app/src/main/java/com/design/demo1/Screens.kt
@@ -1,19 +1,26 @@
 package com.design.demo1
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,29 +29,61 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun Screen() {
     var clicked by rememberSaveable {
-        mutableStateOf(true)
+        mutableIntStateOf(0)
     }
-    Column {
-        if (clicked) {
-            ElevatedButton(
-                modifier = Modifier,
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary
-                ),
-                shape = ShapeDefaults.Medium,
-                elevation = ButtonDefaults.buttonElevation(
-                    defaultElevation = 8.dp,
-                    pressedElevation = 0.dp,
-                    hoveredElevation = 6.dp,
-                ),
-                onClick = { clicked = !clicked }
-            ) {
-                Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
+    var name by rememberSaveable {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        when (clicked) {
+            0 -> {
+                Text(
+                    text = "Hello There!!!",
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                ElevatedButton(
+                    modifier = Modifier,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = MaterialTheme.colorScheme.onPrimary
+                    ),
+                    shape = ShapeDefaults.Medium,
+                    elevation = ButtonDefaults.buttonElevation(
+                        defaultElevation = 8.dp,
+                        pressedElevation = 0.dp,
+                        hoveredElevation = 6.dp,
+                    ),
+                    onClick = { clicked++ }
+                ) {
+                    Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
+                }
             }
-        } else {
-            Text(text = "Hi There!..", fontSize = 40.sp)
-            Text(text = "You Clicked me!!!", fontSize = 40.sp)
+
+            1 -> {
+                TextField(
+                    value = name,
+                    onValueChange = { name = it },
+                    maxLines = 1,
+                    minLines = 1,
+                    placeholder = {
+                        Text(
+                            text = "Enter Your Name"
+                        )
+                    })
+                Button(onClick = { clicked++ }) {
+                    Text(text = "Submit")
+                }
+            }
+
+            else -> {
+                Text(text = "Hi $name", fontSize = 40.sp)
+                Text(text = "Fuck You!!!", fontSize = 40.sp)
+            }
         }
     }
 }
