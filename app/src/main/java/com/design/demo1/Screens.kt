ced797ed80b9a18341333f1e7a1c2420b025064d
ayush <ayushkumar605575@gmail.com>
2024-02-27 21:47:50 +0530

    Ayush

---
+++ app/src/main/java/com/design/demo1/Screens.kt
@@ -0,0 +1,12 @@
+package com.design.demo1
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Screen() {
+    Column {
+        Text(text = "This is a test to know there ")
+    }    
+}
